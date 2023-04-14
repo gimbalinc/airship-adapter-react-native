4bcacbc2120e9b401a1980f5241c7d039fb80dc9
Andrew Tran <andrew.tran@gimbal.com>
2023-04-14 16:37:12 -0700

    feat: refactor transcript container and add event persistence

--- example/src/features/EventTranscript/EventTranscriptContainer.tsx
+++ example/src/features/EventTranscript/EventTranscriptContainer.tsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import AdapterEventUtils from '../../services/AdapterEvents/AdapterEventUtils';
 import type AdapterEvent from '../../services/AdapterEvents/AdapterEvent';
 
@@ -10,34 +11,82 @@ import {
 import GlobalStyles from '../../global/GlobalStyles';
 
 const dummyEvent: AdapterEvent = {
-  description: 'awaiting events',
+  description: 'loading events',
   regionEvent: '',
 };
 
-const EventTranscriptContainer = () => {
-  const [newestEvent, setNewestEvent] = useState(dummyEvent);
+const KEY_EVENTS = 'adapterEventsKey';
 
-  useEffect(() => {
+interface EventTranscriptContainerProps {}
+
+interface EventTranscriptContainerState {
+  events: [AdapterEvent];
+}
+
+export default class EventTranscriptContainer extends Component<
+  EventTranscriptContainerProps,
+  EventTranscriptContainerState
+> {
+  constructor(props: EventTranscriptContainerProps) {
+    super(props);
+
+    this.state = {
+      events: [dummyEvent],
+    };
+
+    this.saveEvent = this.saveEvent.bind(this);
+  }
+
+  componentDidMount(): void {
+    AsyncStorage.getItem(KEY_EVENTS).then((jsonEvents) => {
+      if (jsonEvents == null) {
+        console.log('EventTrascriptContainer no saved events found');
+      } else {
+        const parsedEvents: [AdapterEvent] = JSON.parse(jsonEvents);
+        if (!Array.isArray(parsedEvents)) {
+          throw new Error(`Parsed events in incorrect format: ${jsonEvents}`);
+        }
+
+        this.setState({ events: parsedEvents });
+      }
+    });
+
+    console.log('testest transcript mounting');
     GimbalAirshipAdapter.addListener(RegionEventType.Enter, (event) => {
-      console.log('region enter:', JSON.stringify(event));
       const newEvent = AdapterEventUtils.regionEventToAdapterEvent(event);
-      console.log(`newEvent: ${JSON.stringify(newEvent)}`);
-      setNewestEvent(newEvent);
+      console.log(`newEvent: ${newEvent.description}`);
+      this.saveEvent(newEvent);
     });
 
     GimbalAirshipAdapter.addListener(RegionEventType.Exit, (event) => {
-      console.log('region exit:', JSON.stringify(event));
       const newEvent = AdapterEventUtils.regionEventToAdapterEvent(event);
-      console.log(`newEvent: ${JSON.stringify(newEvent)}`);
-      setNewestEvent(newEvent);
+      console.log(`newEvent: ${newEvent.description}`);
+      this.saveEvent(newEvent);
     });
-  });
+  }
 
-  return (
-    <View style={GlobalStyles.cellContainer}>
-      <Text style={GlobalStyles.rowLabel}>{newestEvent.description}</Text>
-    </View>
-  );
-};
+  saveEvent(event: AdapterEvent) {
+    let events = this.state.events;
+    events.push(event);
+    const updatedJsonEvents = JSON.stringify(events);
+    AsyncStorage.setItem(KEY_EVENTS, updatedJsonEvents)
+      .then(() => {
+        console.log('TESTEST setting events');
+        this.setState({ events });
+      })
+      .catch((error) => {
+        console.log(`Error saving new event ${event}: ${error}`);
+        events.pop();
+      });
+  }
 
-export default EventTranscriptContainer;
+  render() {
+    let lastEvent =
+      this.state.events[this.state.events.length - 1] ?? dummyEvent;
+    return (
+      <View style={GlobalStyles.cellContainer}>
+        <Text style={GlobalStyles.rowLabel}>{lastEvent.description}</Text>
+      </View>
+    );
+  }
+}
