04598ed8266fba5be148624052fad0262b6cdb7c
Andrew Tran <andrew.tran@gimbal.com>
2023-04-28 15:47:09 -0700

    feat: add app icons, fonts, and colors

--- example/src/global/GlobalStyles.tsx
+++ example/src/global/GlobalStyles.tsx
@@ -1,43 +1,50 @@
 import { StyleSheet } from 'react-native';
 
+const color_dark_blue = 'rgb(37 49 59)';
+const color_orange = 'rgb(249 102 85)';
+const color_white = 'rgb(255 255 255)';
+
 const GlobalStyles = StyleSheet.create({
   backgroundContainer: {
     flex: 1,
     flexDirection: 'column',
-    backgroundColor: '#E0A500',
+    backgroundColor: color_white,
   },
   contentContainer: {
     paddingVertical: 20,
     alignItems: 'center',
-    backgroundColor: '#E0A500',
+    backgroundColor: color_white,
   },
   cellContainer: {
     flex: 0,
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#E0A500',
+    backgroundColor: color_white,
     marginTop: 15,
     marginRight: 10,
     marginLeft: 10,
     marginBottom: 10,
   },
   text: {
+    fontFamily: 'BrandonText-Regular',
     fontSize: 16,
-    color: '#0d6a83',
+    color: color_dark_blue,
     textAlign: 'center',
     padding: 10,
   },
   header: {
+    fontFamily: 'BrandonText-Bold',
     fontSize: 24,
     fontWeight: 'bold',
-    color: '#0d6a83',
+    color: color_orange,
     textAlign: 'center',
     padding: 10,
   },
   rowLabel: {
+    fontFamily: 'BrandonText-Light',
     flexDirection: 'row',
-    color: '#0d6a83',
+    color: color_dark_blue,
     fontSize: 16,
     marginRight: 10,
   },
