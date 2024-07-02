53d3d0dc02b815f44f3371a373c653f4cfbf9e6f
Andrew Tran <andrew.tran@infillion.com>
2024-07-02 16:59:30 -0700

    fix: android example app namespacing issues

--- example/android/app/src/main/java/com/gimbal/airship/reactnative/android/MainApplication.java
+++ example/android/app/src/main/java/com/gimbal/airship/reactnative/android/MainApplication.java
@@ -1,6 +1,7 @@
-package com.rtngimbalairshipadapterexample;
+package com.gimbal.airship.reactnative.android;
 
 import android.app.Application;
+
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
@@ -8,6 +9,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
+import com.rtngimbalairshipadapterexample.ReactNativeFlipper;
+
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
