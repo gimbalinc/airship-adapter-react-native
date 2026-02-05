fe224fb35ae3be107cdc795bb3d4e20c072a6395
Ryan Lepinski <ryan.lepinski@airship.com>
2026-02-05 13:48:03 -0800

    Update iOS to Airship SDK 20

--- ios/RtnGimbalAirshipAdapter.h
+++ ios/RtnGimbalAirshipAdapter.h
@@ -2,16 +2,7 @@
 #import "VisitEvent.h"
 #import "GimbalService.h"
 #import "AdapterEventEmitter.h"
-
-#ifdef RCT_NEW_ARCH_ENABLED
 #import "RNRtnGimbalAirshipAdapterSpec.h"
 
-@interface RtnGimbalAirshipAdapter : NSObject <NativeGimbalAirshipAdapterSpec,GMBLPlaceManagerDelegate>
-#else
-#import <React/RCTBridgeModule.h>
-#import <Gimbal/Gimbal.h>
-
-@interface RtnGimbalAirshipAdapter : RCTEventEmitter <RCTBridgeModule, GMBLPlaceManagerDelegate>
-#endif
-
+@interface RtnGimbalAirshipAdapter : NSObject <NativeGimbalAirshipAdapterModuleSpec, GMBLPlaceManagerDelegate>
 @end
